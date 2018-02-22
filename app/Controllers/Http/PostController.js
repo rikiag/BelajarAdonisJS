2c3098359a31805d2195ceaa89e96b7bc9a57c9a
Riki Agusnaidi <r.agusnaidi@gmail.com>
2018-02-22 16:12:45 +0700

    Read Data from databases

--- app/Controllers/Http/PostController.js
+++ app/Controllers/Http/PostController.js
@@ -1,6 +1,18 @@
 'use strict'
 
+//Bring in Model
+const Post = use('App/Models/Post')
+
 class PostController {
+	async index({ view }){
+
+		const posts = await Post.all()
+
+		return view.render('home', {
+			title: 'Home',
+			posts: posts.toJSON()
+		})
+	}
 }
 
 module.exports = PostController
--- start/routes.js
+++ start/routes.js
@@ -15,4 +15,6 @@
 
 const Route = use('Route')
 
-Route.on('/').render('home')
+// Route.on('/').render('home')
+
+Route.get('/', 'PostController.index')
